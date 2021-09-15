89aa3938a04eb70d281e12f96adacb4d4f4fc9e7
Chris-Yarnot <86123295+Chris-Yarnot@users.noreply.github.com>
2021-09-15 10:51:55 -0500

    Commented Complex code

--- 2021_09_13-17/JavaScript_Answers.js
+++ 2021_09_13-17/JavaScript_Answers.js
@@ -1,12 +1,20 @@
 function longestNonrepeatingSubstring(str){
+  //we want to iterate through the loop one more time than the length of the string
   str+= str+"-";
+  //previous index of the char with cooresponding ascii value
   let lastIndex= new Array(128).fill(0);
+  //the closest index of a repeated character to the current index
   let minStartIndex= 0;
+  //the maximum length string found so far
   let maxLen=0;
+  //the start index of the maximum length string found so far
   let maxStart= 0;
+  //the end index of the maximum length string found so far
   let maxEnd= 0;
   for (let i= 1; i<str.length; i++) {
-    let i2= str[i].charCodeAt(0);
+    	//ascii value of current character
+	let i2= str[i].charCodeAt(0);
+	//if the string ended at this char where would it need to start to not have repeating characters
 	let startIndexEndHere= lastIndex[i2];
 	if(startIndexEndHere < minStartIndex){
 		startIndexEndHere= minStartIndex;
@@ -21,14 +29,14 @@ function longestNonrepeatingSubstring(str){
 		maxEnd= i;
 	}
   }
-	return str.substring(maxStart, maxEnd);
+  return str.substring(maxStart, maxEnd);
 }
-console.log(longestNonrepeatingSubstring("Hello World!"));
+console.log(longestNonrepeatingSubstring("Hello World!"));// --> " World!"
 
 console.log(longestNonrepeatingSubstring("abcabcbb"));// --> "abc"
 
 console.log(longestNonrepeatingSubstring("aaaaaa"));// --> "a"
 
-console.log(longestNonrepeatingSubstring("abcde"));// --> "abcde"
+console.log(longestNonrepeatingSubstring("ab-cde"));// --> "ab-cde"
 
 console.log(longestNonrepeatingSubstring("abcda"));// --> "abcd"
