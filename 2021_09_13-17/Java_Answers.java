a4f2c98d80b4c2769abd7493819a760e130cca41
Chris-Yarnot <86123295+Chris-Yarnot@users.noreply.github.com>
2021-09-15 11:11:59 -0500

    fixed indentation

--- 2021_09_13-17/Java_Answers.java
+++ 2021_09_13-17/Java_Answers.java
@@ -1,17 +1,17 @@
 public class Java_Answers {
 	public static String rot13(String s){
-        String output= "";
-        for(int i= 0; i<s.length(); i++){
-            char c= s.charAt(i);
-            if((c>='a' && c<='m') || (c>='A' && c<='M')){
-                c+= 13;
-            }else if((c>='n' && c<='z') || (c>='N' && c<='Z')){
-                c-= 13;
-            }
-            output= output + c;
-        }
-        return output;
-    }
+		String output= "";
+		for(int i= 0; i<s.length(); i++){
+		    char c= s.charAt(i);
+		    if((c>='a' && c<='m') || (c>='A' && c<='M')){
+			c+= 13;
+		    }else if((c>='n' && c<='z') || (c>='N' && c<='Z')){
+			c-= 13;
+		    }
+		    output= output + c;
+		}
+		return output;
+	}
 	
 	public static String digits= "0123456789abcdefghijklmnopqrstuvwxyz";
 	public static int base= 8;
@@ -27,8 +27,8 @@ public class Java_Answers {
 		}while(i != 0);
 		return output;
 	}
-    public static void main(String args[]) {
-      System.out.println(rot13("test"));
-      System.out.println(changeBase(42));
-    }
+	public static void main(String args[]) {
+		System.out.println(rot13("test"));
+		System.out.println(changeBase(42));
+	}
 }
