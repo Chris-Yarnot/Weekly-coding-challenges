d82b79057737d61e89acb06a8158c711aada037f
Chris-Yarnot <86123295+Chris-Yarnot@users.noreply.github.com>
2021-09-15 11:09:08 -0500

    Minor bug fix
    when output ended at the same time as the input string would occasionally add garbage characters to the end of the output. this is now fixed

--- 2021_09_13-17/JavaScript_Answers.js
+++ 2021_09_13-17/JavaScript_Answers.js
@@ -1,17 +1,15 @@
 function longestNonrepeatingSubstring(str){
-  //we want to iterate through the loop one more time than the length of the string
-  str+= str+"-";
   //previous index of the char with cooresponding ascii value
   let lastIndex= new Array(128).fill(0);
   //the closest index of a repeated character to the current index
   let minStartIndex= 0;
   //the maximum length string found so far
-  let maxLen=0;
+  let maxLen=-1;
   //the start index of the maximum length string found so far
   let maxStart= 0;
   //the end index of the maximum length string found so far
   let maxEnd= 0;
-  for (let i= 1; i<str.length; i++) {
+  for (let i= 0; i<str.length; i++) {
     	//ascii value of current character
 	let i2= str[i].charCodeAt(0);
 	//if the string ended at this char where would it need to start to not have repeating characters
@@ -26,17 +24,17 @@ function longestNonrepeatingSubstring(str){
 	if(maxLen < len){
 		maxLen= len;
 		maxStart= startIndexEndHere;
-		maxEnd= i;
+		maxEnd= i+1;
 	}
   }
   return str.substring(maxStart, maxEnd);
 }
-console.log(longestNonrepeatingSubstring("Hello World!"));// --> " World!"
+console.log(longestNonrepeatingSubstring("Hello World"));// --> " World"
 
 console.log(longestNonrepeatingSubstring("abcabcbb"));// --> "abc"
 
 console.log(longestNonrepeatingSubstring("aaaaaa"));// --> "a"
 
-console.log(longestNonrepeatingSubstring("ab-cde"));// --> "ab-cde"
+console.log(longestNonrepeatingSubstring("abcdef"));// --> "abcdef"
 
 console.log(longestNonrepeatingSubstring("abcda"));// --> "abcd"
